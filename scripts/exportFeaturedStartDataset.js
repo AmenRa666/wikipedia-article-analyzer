ade51320b266e4b82504da482c393c79cc32787f
Elias Bassani <elias@Eliass-MBP.lan>
2016-10-28 15:12:35 +0200

    Commit prima di morire

---
+++ scripts/exportFeaturedStartDataset.js
@@ -0,0 +1,52 @@
+// MODULES
+var json2csv = require('json2csv');
+var fs = require('fs');
+var mongoose = require('mongoose')
+var math = require('mathjs')
+// Open a connection to the database on our locally running instance of MongoDB
+mongoose.connect('mongodb://localhost/wikipedia')
+
+// MODEL
+var Article = require('../models/article.js').Article
+
+var fields = ["characterCount", "wordCount", "syllableCount", "sentenceCount", "sectionCount", "subsectionCount", "paragraphCount", "meanSectionSize", "meanParagraphSize", "largestSectionSize", "shortestSectionSize", "largestShortestSectionRatio", "sectionSizeStandardDeviation", "meanOfSubsectionsPerSection", "abstractSize", "abstractSizeArtcileLengthRatio", "citationCount", "citationCountPerSentence", "citationCountPerSection", "externalLinksCount", "externalLinksPerSentence", "externalLinksPerSection", "imageCount", "imagePerSentence", "imagePerSection", "meanSentenceSize", "largestSentenceSize", "shortestSentenceSize", "largeSentenceRate", "shortSentenceRate", "questionCount", "questionRatio", "exclamationCount", "exclamationRatio", "toBeVerbCount", "toBeVerbRatio", "toBeVerbPerSentence", "toBeVerbRate", "automatedReadabilityIndex", "colemanLiauIndex", "fleshReadingEase", "fleschKincaidGradeLevel", "gunningFogIndex", "lasbarhetsIndex", "smogGrading", "linsearWriteFormula", "daleChallReadabilityFormula", "differentWordCount", "differentWordsPerSentence", "differentWordsRate", "nounCount", "nounsPerSentence", "nounsRate", "differentNounCount", "differentNounsPerSentence", "differentNounsRate", "verbCount", "verbsPerSentence", "verbsRate", "differentVerbCount", "differentVerbsPerSentence", "differentVerbsRate", "pronounCount", "pronounsPerSentence", "pronounsRate", "differentPronounCount", "differentPronounsPerSentence", "differentPronounsRate", "adjectiveCount", "adjectivesPerSentence", "differentAdjectiveCount", "differentAdjectivesPerSentence", "differentAdjectivesRate", "adverbCount", "adverbsPerSentence", "adverbsRate", "differentAdverbCount", "differentAdverbsPerSentence", "differentAdverbsRate", "coordinatingConjunctionCount", "coordinatingConjunctionsPerSentence", "coordinatingConjunctionsRate", "differentCoordinatingConjunctionCount", "differentCoordinatingConjunctionsPerSentence", "differentCoordinatingConjunctionsRate", "subordinatingPrepositionAndConjunctionCount", "subordinatingPrepositionsAndConjunctionsPerSentence", "subordinatingPrepositionsAndConjunctionsRate", "differentSubordinatingPrepositionAndConjunctionCount", "differentSubordinatingPrepositionsAndConjunctionsPerSentence", "differentSubordinatingPrepositionsAndConjunctionsRate", "syllablesPerWord", "charactersPerWord", "qualityClass"]
+
+// get notified if we connect successfully or if a connection error occurs
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log('Connected to MongoDB')
+
+  Article.find({}, (err, docs) => {
+    if (err) console.log(err);
+    else if (docs) {
+
+      for (var i = 0; i < docs.length; i++) {
+        if (docs[i].qualityClass != 7 && docs[i].qualityClass != 2) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+      }
+
+      for (var i = 0; i < docs.length; i++) {
+        if (docs[i].qualityClass == 7) {
+          docs[i].qualityClass = 1
+        }
+      }
+
+      console.log(docs.length);
+
+      var csv = json2csv({ data: docs, fields: fields })
+      fs.writeFile('featuredStartDataset.csv', csv, function(err) {
+        if (err) throw err;
+        console.log('CSV Saved!');
+        process.exit()
+      });
+
+    }
+    else {
+      console.log('Not Found');
+      process.exit()
+    }
+  })
+})
---
+++ scripts/exportTwoClassDatasetToCSV.js
@@ -0,0 +1,97 @@
+// MODULES
+var json2csv = require('json2csv');
+var fs = require('fs');
+var mongoose = require('mongoose')
+var math = require('mathjs')
+// Open a connection to the database on our locally running instance of MongoDB
+mongoose.connect('mongodb://localhost/wikipedia')
+
+// MODEL
+var Article = require('../models/article.js').Article
+
+var fields = ["characterCount", "wordCount", "syllableCount", "sentenceCount", "sectionCount", "subsectionCount", "paragraphCount", "meanSectionSize", "meanParagraphSize", "largestSectionSize", "shortestSectionSize", "largestShortestSectionRatio", "sectionSizeStandardDeviation", "meanOfSubsectionsPerSection", "abstractSize", "abstractSizeArtcileLengthRatio", "citationCount", "citationCountPerSentence", "citationCountPerSection", "externalLinksCount", "externalLinksPerSentence", "externalLinksPerSection", "imageCount", "imagePerSentence", "imagePerSection", "meanSentenceSize", "largestSentenceSize", "shortestSentenceSize", "largeSentenceRate", "shortSentenceRate", "questionCount", "questionRatio", "exclamationCount", "exclamationRatio", "toBeVerbCount", "toBeVerbRatio", "toBeVerbPerSentence", "toBeVerbRate", "automatedReadabilityIndex", "colemanLiauIndex", "fleshReadingEase", "fleschKincaidGradeLevel", "gunningFogIndex", "lasbarhetsIndex", "smogGrading", "linsearWriteFormula", "daleChallReadabilityFormula", "differentWordCount", "differentWordsPerSentence", "differentWordsRate", "nounCount", "nounsPerSentence", "nounsRate", "differentNounCount", "differentNounsPerSentence", "differentNounsRate", "verbCount", "verbsPerSentence", "verbsRate", "differentVerbCount", "differentVerbsPerSentence", "differentVerbsRate", "pronounCount", "pronounsPerSentence", "pronounsRate", "differentPronounCount", "differentPronounsPerSentence", "differentPronounsRate", "adjectiveCount", "adjectivesPerSentence", "differentAdjectiveCount", "differentAdjectivesPerSentence", "differentAdjectivesRate", "adverbCount", "adverbsPerSentence", "adverbsRate", "differentAdverbCount", "differentAdverbsPerSentence", "differentAdverbsRate", "coordinatingConjunctionCount", "coordinatingConjunctionsPerSentence", "coordinatingConjunctionsRate", "differentCoordinatingConjunctionCount", "differentCoordinatingConjunctionsPerSentence", "differentCoordinatingConjunctionsRate", "subordinatingPrepositionAndConjunctionCount", "subordinatingPrepositionsAndConjunctionsPerSentence", "subordinatingPrepositionsAndConjunctionsRate", "differentSubordinatingPrepositionAndConjunctionCount", "differentSubordinatingPrepositionsAndConjunctionsPerSentence", "differentSubordinatingPrepositionsAndConjunctionsRate", "syllablesPerWord", "charactersPerWord", "qualityClass"]
+
+// get notified if we connect successfully or if a connection error occurs
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log('Connected to MongoDB')
+
+  Article.find({}, (err, docs) => {
+    if (err) console.log(err);
+    else if (docs) {
+
+      var aClassCount = 0
+      var gaClassCount = 0
+      var bClassCount = 0
+      var cClassCount = 0
+      var startClassCount = 0
+      var stubClassCount = 0
+
+      for (var i = 0; i < docs.length; i++) {
+        if (docs[i].qualityClass == 1) {
+          stubClassCount++
+        }
+        else if (docs[i].qualityClass == 2) {
+          startClassCount++
+        }
+        else if (docs[i].qualityClass == 3) {
+          cClassCount++
+        }
+        else if (docs[i].qualityClass == 4) {
+          bClassCount++
+        }
+        else if (docs[i].qualityClass == 5) {
+          gaClassCount++
+        }
+        else if (docs[i].qualityClass == 6) {
+          aClassCount++
+        }
+        if (docs[i].qualityClass == 1 && stubClassCount > 17) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+        else if (docs[i].qualityClass == 2 && startClassCount > 17) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+        else if (docs[i].qualityClass == 3 && cClassCount > 16) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+        else if (docs[i].qualityClass == 4 && bClassCount > 17) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+        else if (docs[i].qualityClass == 5 && gaClassCount > 17) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+        else if (docs[i].qualityClass == 6 && aClassCount > 16) {
+          docs.splice(docs.indexOf(docs[i]), 1)
+          i--
+        }
+      }
+
+      for (var i = 0; i < docs.length; i++) {
+        if (docs[i].qualityClass != 7) {
+          docs[i].qualityClass = 1
+        }
+      }
+
+      console.log(docs.length);
+
+      var csv = json2csv({ data: docs, fields: fields })
+      fs.writeFile('twoClassDataset.csv', csv, function(err) {
+        if (err) throw err;
+        console.log('CSV Saved!');
+        process.exit()
+      });
+
+    }
+    else {
+      console.log('Not Found');
+      process.exit()
+    }
+  })
+})
